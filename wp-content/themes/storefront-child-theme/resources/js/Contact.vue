d808092dc33b2e8feabf667e65a3978a9e81af35
Kevin Lim <zzkevinlim@gmail.com>
2021-03-21 11:47:25 +1300

    202103211147

--- wp-content/themes/storefront-child-theme/resources/js/Contact.vue
+++ wp-content/themes/storefront-child-theme/resources/js/Contact.vue
@@ -33,8 +33,14 @@
               </div>
             </div>
           </div>
-          <div class="row-span-1 flex justify-end">
-            <div class="flex items-center justify-center font-jumble text-[12px] text-green-01 bg-transparent uppercase py-[4px] px-[8px] border-2 border-green-01 rounded-[15px] transition-all duration-300 hover:bg-yellow-01 hover:border-yellow-01 hover:text-purple-01 hover:transform hover:-translate-y-2 hover:translate-x-2 hover:shadow-xl cursor-pointer" @click="onSubmit">{{ enquiry_submit_label }}</div>
+          <div class="row-span-1 flex items-center justify-end">
+            <svg v-if="loading" class="animate-spin -ml-1 mr-3 h-5 w-5 text-green-01" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
+            <div class="flex items-center justify-center font-jumble text-[12px] text-green-01 bg-transparent uppercase py-[4px] px-[8px] border-2 border-green-01 rounded-[15px] transition-all duration-300 hover:bg-yellow-01 hover:border-yellow-01 hover:text-purple-01 hover:transform hover:-translate-y-2 hover:translate-x-2 hover:shadow-xl cursor-pointer" @click="onSubmit">
+              {{ enquiry_submit_label }}
+            </div>
           </div>
         </div>
       </form>
@@ -80,7 +86,9 @@ export default {
     ValidationObserver,
   },
   data () {
-    return {};
+    return {
+      loading: false,
+    };
   },
   computed: {
     ...mapState({
@@ -130,7 +138,7 @@ export default {
     onSubmit () {
       this.$refs.form.validate().then((success) => {
         if (success) {
-          console.log(this.$store);
+          this.loading = true;
           let formData = new FormData();
           formData.append('nonce', this.wp_nonce);
           formData.append('your-name', this.your_name);
@@ -154,9 +162,11 @@ export default {
             } else {
               this.$swal('Error', response.data.message, 'error');
             }
+            this.loading = false;
           });
         }
       });
+      this.loading = false;
     }
   }
 };
