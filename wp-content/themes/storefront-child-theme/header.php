c050cbedce6be7486b16c38914f124351c00af80
Kevin Lim <zzkevinlim@gmail.com>
2021-06-24 14:26:43 +1200

    202106241426
    Took 3 minutes

--- wp-content/themes/storefront-child-theme/header.php
+++ wp-content/themes/storefront-child-theme/header.php
@@ -1,8 +1,12 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php echo get_bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property='og:title' content='<?php echo get_bloginfo( 'name' ) ?>'/>
+    <meta property='og:image' content='<?php echo get_field('banner_image', '13')['url'] ?>'/>
+    <meta property='og:description' content='<?php echo get_bloginfo( 'description' ) ?>'/>
+    <meta property='og:url' content='<?php echo get_bloginfo( 'wpurl' ) ?>'/>
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
